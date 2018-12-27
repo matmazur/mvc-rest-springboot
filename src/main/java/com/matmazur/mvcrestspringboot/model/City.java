f173f44a571dcaeee724ed7ad0b05e579411b95c
Bingo Bobo <matmazur90@gmail.com>
2018-12-27 15:53:34 +0100

    added new POJO class with builder

---
+++ src/main/java/com/matmazur/mvcrestspringboot/model/City.java
@@ -0,0 +1,55 @@
+package com.matmazur.mvcrestspringboot.model;
+
+public class City {
+
+    private String name;
+    private long population;
+
+    public City(String name, long population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public City() {
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    public static class Builder {
+
+        private String name;
+        private long population;
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withName(long population) {
+            this.population = population;
+            return this;
+        }
+
+        public City build() {
+            return new City(name, population);
+        }
+    }
+}
