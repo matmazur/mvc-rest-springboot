3547557c99051ff4a5bb309ef343ce4f9e2bd7a9
Bingo Bobo <matmazur90@gmail.com>
2018-12-27 15:48:27 +0100

    initial commit

---
+++ src/main/java/com/matmazur/mvcrestspringboot/MvcRestSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.mvcrestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRestSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcRestSpringbootApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/matmazur/mvcrestspringboot/MvcRestSpringbootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.matmazur.mvcrestspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvcRestSpringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
