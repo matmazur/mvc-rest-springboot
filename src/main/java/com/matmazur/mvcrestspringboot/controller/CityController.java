d49b8a2a8ad50340e038adf35d2f5647d985fb24
Bingo Bobo <matmazur90@gmail.com>
2019-01-02 15:40:48 +0100

    few small fixes to the previous build

--- src/main/java/com/matmazur/mvcrestspringboot/controller/CityController.java
+++ src/main/java/com/matmazur/mvcrestspringboot/controller/CityController.java
@@ -3,12 +3,13 @@ package com.matmazur.mvcrestspringboot.controller;
 import com.matmazur.mvcrestspringboot.model.CityWrapper;
 import com.matmazur.mvcrestspringboot.model.City;
 import org.springframework.beans.support.PagedListHolder;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -50,25 +51,25 @@ public class CityController {
     @RequestMapping
     public CityWrapper getCities(@RequestParam(required = false) Integer page, @RequestParam(required = false) String order) {
 
-        if (order.equals("asc")) {
-            cities.sort(Comparator.comparing(City::getName));
-        } else if (order.equals("desc")) {
-            cities.sort(Comparator.comparing(City::getName).reversed());
+        if (order != null) {
+            if (order.equals("asc")) {
+                cities.sort(Comparator.comparing(City::getName));
+            } else if (order.equals("desc")) {
+                cities.sort(Comparator.comparing(City::getName).reversed());
+            }
         }
 
+        CityWrapper cityWrapper = new CityWrapper();
         int pageSize = 5;
-        if (page != null && (page > 0 && page < Math.round((double) cities.size() / pageSize) + 1)) {
+        if (page != null) {
             PagedListHolder<City> pagedListHolder = new PagedListHolder<>(cities);
             pagedListHolder.setPageSize(pageSize);
-            pagedListHolder.setPage(page - 1);
-            CityWrapper cityWrapper = new CityWrapper();
+            pagedListHolder.setPage(page);
             cityWrapper.setCities(pagedListHolder.getPageList());
             return cityWrapper;
         }
 
-        CityWrapper cityWrapper = new CityWrapper();
         cityWrapper.setCities(cities);
-
         return cityWrapper;
     }
 
@@ -77,15 +78,13 @@ public class CityController {
 
         return cities.stream().filter(x -> x.getId() == id).findAny().orElse(null);
     }
-}
 
-//    @RequestMapping(value = "/cities")
-//    public ResponseEntity<List<City>> getCities() {
-//
-//        return new ResponseEntity<>(Arrays.asList(
-//                new City("Gdansk", 200_000),
-//                new City("Warszawa", 600_000),
-//                new City("Poznan", 300_000),
-//                new City("Krakow", 400_000)),
-//                HttpStatus.OK);
-//    }
+    @PostConstruct
+    public void starter(){
+        System.out.println("start");
+    }
+    @PreDestroy
+    public void destr(){
+        System.out.println("destroy");
+    }
+}
